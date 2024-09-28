327b5b04290ce0074afac91fb6daa584e81cf12a
Chris Tanner <ctanneraudio@gmail.com>
2024-09-28 12:54:21 -0400

    Ignore case

---
+++ tests/Database/Factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CTanner\LaravelDeepSync\Tests\Database\Factories;
+
+use CTanner\LaravelDeepSync\Tests\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(),
+            'body' => fake()->paragraph(),
+        ];
+    }
+}
